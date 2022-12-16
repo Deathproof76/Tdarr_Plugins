6637422230ab510927f442dca467116eb3ed0258
Deathproof76 <95944496+Deathproof76@users.noreply.github.com>
2022-12-16 17:51:26 +0100

    Create Tdarr_Plugin_DTPF_rename_files_after_repair.js

---
+++ Community/Tdarr_Plugin_DTPF_rename_files_after_repair.js
@@ -0,0 +1,55 @@
+/* eslint-disable */
+
+// tdarrSkipTest
+const details = () => {
+  return {
+    id: "Tdarr_Plugin_DTPF_rename_files_after_repair",
+    Stage: "Post-processing",
+    Name: "Rename repaired File",
+    Type: "Video",
+    Operation: "Transcode",
+    Description: `[Contains built-in filter] If the filename contains '_i_frame_issue', this plugin removes this, ideally after being repaired. \n\n`,
+    Version: "1.00",
+    Tags: "post-processing",
+    Inputs:[]
+  };
+};
+
+// eslint-disable-next-line no-unused-vars
+const plugin = (file, librarySettings, inputs, otherArguments) => {
+    
+    const lib = require('../methods/lib')();
+  // eslint-disable-next-line no-unused-vars,no-param-reassign
+  inputs = lib.loadDefaultValues(inputs, details);
+  try {
+    var fs = require("fs");
+    var fileNameOld = file._id;
+
+    if (
+      file.file.includes("i_frame_issue")
+    ) {
+      file.file = file.file.replace("_i_frame_issue", "_healed");
+    }
+    
+    if (fileNameOld != file._id) {
+      fs.renameSync(fileNameOld, file._id, {
+        overwrite: true,
+      });
+
+      var response = {
+        file,
+        removeFromDB: false,
+        updateDB: true,
+      };
+
+      return response;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+
+ 
+module.exports.details = details;
+module.exports.plugin = plugin;
